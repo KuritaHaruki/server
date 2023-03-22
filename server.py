16bf5fa519d485e2db3321bc77f25559c3b807bd
KuritaHaruki <106136720+KuritaHaruki@users.noreply.github.com>
2023-03-23 03:17:49 +0900

    Add files via upload

--- server.py
+++ server.py
@@ -1,20 +1,20 @@
 from flask import Flask, request, render_template
+import math
 app = Flask(__name__)
 file_path = "./sensor_data.csv"
 port_num = 20080
 
 @app.route('/', methods=['GET'])
 def get_html():
-    return render_template('./index.html')
+    return render_template('./test.html')
 
 
 @app.route('/lux', methods=['POST'])
 def update_lux():
-    time = request.form["time"]
     lux = request.form["lux"]
     try:
-        f = open(file_path, 'w')
-        f.write(time + "," + lux)
+        f = open(file_path, 'a')
+        f.write(lux + " ")
         return "succeeded to write"
     except Exception as e:
         print(e)
@@ -24,14 +24,28 @@ def update_lux():
 
 @app.route('/lux', methods=['GET'])
 def get_lux():
+    split_lux = []
+    float_lux = []
+
     try:
         f = open(file_path, 'r')
         for row in f:
             lux = row
+
+        split_lux = lux.split()
+        for item in split_lux:
+            float_lux.append(float(item))
+
+        mean = sum(float_lux)/len(float_lux)
+        mean_adjust = round(mean,2)
+        mean_adjust = str(mean_adjust)
+
     except Exception as e:
         print(e)
     finally:
         f.close()
-        return lux
+        f = open(file_path, 'w')
+        f.close()
+        return mean_adjust
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=port_num)
